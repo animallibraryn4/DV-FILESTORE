159145a8c9b168fc144e1f3e280d05505cac303f
animallibraryn4 <nikhil7858978052@gmail.com>
2025-01-26 13:53:25 +0530

    Update commands.py

--- clone_plugins/commands.py
+++ clone_plugins/commands.py
@@ -1,253 +1,116 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-FILESTORE > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-FILESTORE/blob/master/LICENSE >
-#
-# All rights reserved.
-
-import os
-import logging
-import random
-import asyncio
-from Script import script
-from validators import domain
-from clone_plugins.dbusers import db
-from clone_plugins.users_api import get_user, update_user_info
-from pyrogram import Client, filters, enums
-from plugins.database import get_file_details
-from pyrogram.errors import ChatAdminRequired, FloodWait
-from config import BOT_USERNAME, ADMINS
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery, InputMediaPhoto
-from config import PICS, CUSTOM_FILE_CAPTION, AUTO_DELETE_TIME, AUTO_DELETE
-import re
-import json
-import base64
-from config import DB_URI as MONGO_URL
-from pymongo import MongoClient
-
-mongo_client = MongoClient(MONGO_URL)
-mongo_db = mongo_client["cloned_erabot"]
-
-logger = logging.getLogger(__name__)
-
-# This file is part of < https://github.com/IamDvis/DV-FILESTORE > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-FILESTORE/blob/master/LICENSE >
-
-def get_size(size):
-    """Get size in readable format"""
-
-    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
-    size = float(size)
-    i = 0
-    while size >= 1024.0 and i < len(units):
-        i += 1
-        size /= 1024.0
-    return "%.2f %s" % (size, units[i])
-
-# This file is part of < https://github.com/IamDvis/DV-FILESTORE > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-FILESTORE/blob/master/LICENSE >
-
-@Client.on_message(filters.command("start") & filters.incoming)
-async def start(client, message):
-    if not await db.is_user_exist(message.from_user.id):
-        await db.add_user(message.from_user.id, message.from_user.first_name)
-    if len(message.command) != 2:
-        buttons = [[
-            InlineKeyboardButton('💝 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/+7ehnJA3aMb84OGNl')
-            ],[
-            InlineKeyboardButton('🤖 ᴄʀᴇᴀᴛᴇ ʏᴏᴜʀ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ', url=f'https://t.me/{BOT_USERNAME}?start=clone')
-            ],[
-            InlineKeyboardButton('💁‍♀️ ʜᴇʟᴘ', callback_data='help'),
-            InlineKeyboardButton('ᴀʙᴏᴜᴛ 🔻', callback_data='about')
-        ]]
-        me2 = (await client.get_me()).mention
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.CLONE_START_TXT.format(message.from_user.mention, me2),
-            reply_markup=reply_markup
-        )
-        return
-
-# This file is part of < https://github.com/IamDvis/DV-FILESTORE > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-FILESTORE/blob/master/LICENSE >
-    
-    data = message.command[1]
-    try:
-        pre, file_id = data.split('_', 1)
-    except:
-        file_id = data
-        pre = ""   
-
-    files_ = await get_file_details(file_id)           
-    if not files_:
-        pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
-        try:
-            msg = await client.send_cached_media(
-                chat_id=message.from_user.id,
-                file_id=file_id,
-                protect_content=True if pre == 'filep' else False,
-                )
-            filetype = msg.media
-            file = getattr(msg, filetype.value)
-            title = '@ai_image_junction  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
-            size=get_size(file.file_size)
-            f_caption = f"<code>{title}</code>"
-            if CUSTOM_FILE_CAPTION:
-                try:
-                    f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='')
-                except:
-                    return
-            await msg.edit_caption(f_caption)
-            k = await msg.reply(f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>{AUTO_DELETE} mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</i></b>",quote=True)
-            await asyncio.sleep(AUTO_DELETE_TIME)
-            await msg.delete()
-            await k.edit_text("<b>Your File/Video is successfully deleted!!!</b>")
-            return
-        except:
-            pass
-        return await message.reply('No such file exist.')
-    files = files_[0]
-    title = files.file_name
-    size=get_size(files.file_size)
-    f_caption=files.caption
-    if CUSTOM_FILE_CAPTION:
-        try:
-            f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
-        except Exception as e:
-            logger.exception(e)
-            f_caption=f_caption
-    if f_caption is None:
-        f_caption = f"{files.file_name}"
-    await client.send_cached_media(
-        chat_id=message.from_user.id,
-        file_id=file_id,
-        caption=f_caption,
-        protect_content=True if pre == 'filep' else False,
-        )
-
-# This file is part of < https://github.com/IamDvis/DV-FILESTORE > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-FILESTORE/blob/master/LICENSE >
-
-@Client.on_message(filters.command('api') & filters.private)
-async def shortener_api_handler(client, m: Message):
-    user_id = m.from_user.id
-    user = await get_user(user_id)
-    cmd = m.command
-
-    if len(cmd) == 1:
-        s = script.SHORTENER_API_MESSAGE.format(base_site=user["base_site"], shortener_api=user["shortener_api"])
-        return await m.reply(s)
-
-    elif len(cmd) == 2:    
-        api = cmd[1].strip()
-        await update_user_info(user_id, {"shortener_api": api})
-        await m.reply("Shortener API updated successfully to " + api)
-    else:
-        await m.reply("You are not authorized to use this command.")
-
-# This file is part of < https://github.com/IamDvis/DV-FILESTORE > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-FILESTORE/blob/master/LICENSE >
-
-@Client.on_message(filters.command("base_site") & filters.private)
-async def base_site_handler(client, m: Message):
-    user_id = m.from_user.id
-    user = await get_user(user_id)
-    cmd = m.command
-    text = f"/base_site (base_site)\n\nCurrent base site: None\n\n EX: /base_site shortnerdomain.com\n\nIf You Want To Remove Base Site Then Copy This And Send To Bot - `/base_site None`"
-    
-    if len(cmd) == 1:
-        return await m.reply(text=text, disable_web_page_preview=True)
-    elif len(cmd) == 2:
-        base_site = cmd[1].strip()
-        if not domain(base_site):
-            return await m.reply(text=text, disable_web_page_preview=True)
-        await update_user_info(user_id, {"base_site": base_site})
-        await m.reply("Base Site updated successfully")
-    else:
-        await m.reply("You are not authorized to use this command.")
-
-# This file is part of < https://github.com/IamDvis/DV-FILESTORE > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-FILESTORE/blob/master/LICENSE >
-
-
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
-    if query.data == "close_data":
-        await query.message.delete()
-    elif query.data == "start":
+    # Handling the 'start' command button
+    if query.data == "start":
         buttons = [[
-            InlineKeyboardButton('💝 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/+7ehnJA3aMb84OGNl')
-            ],[
-            InlineKeyboardButton('🤖 ᴄʀᴇᴀᴛᴇ ʏᴏᴜʀ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ', url=f'https://t.me/{BOT_USERNAME}?start=clone')
-            ],[
-            InlineKeyboardButton('💁‍♀️ ʜᴇʟᴘ', callback_data='help'),
-            InlineKeyboardButton('ᴀʙᴏᴜᴛ 🔻', callback_data='about')
+            InlineKeyboardButton('/start', callback_data='start'),
+            InlineKeyboardButton('/link', callback_data='link')
+        ], [
+            InlineKeyboardButton('/base_site', callback_data='base_site'),
+            InlineKeyboardButton('/api', callback_data='api')
+        ], [
+            InlineKeyboardButton('/deletecloned', callback_data='deletecloned'),
+            InlineKeyboardButton('/broadcast', callback_data='broadcast')
+        ], [
+            InlineKeyboardButton('💝 Support Group', url='https://t.me/+7ehnJA3aMb84OGNl'),
+            InlineKeyboardButton('💁‍♀️ Help', callback_data='help')
         ]]
         
         reply_markup = InlineKeyboardMarkup(buttons)
         await client.edit_message_media(
-            query.message.chat.id, 
-            query.message.id, 
+            query.message.chat.id,
+            query.message.id,
             InputMediaPhoto(random.choice(PICS))
         )
-        me2 = (await client.get_me()).mention
         await query.message.edit_text(
             text=script.CLONE_START_TXT.format(query.from_user.mention, me2),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
 
-# This file is part of < https://github.com/IamDvis/DV-FILESTORE > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-FILESTORE/blob/master/LICENSE >
-
-    elif query.data == "help":
+    # Handle /link command
+    elif query.data == "link":
         buttons = [[
-            InlineKeyboardButton('Hᴏᴍᴇ', callback_data='start'),
-            InlineKeyboardButton('🔒 Cʟᴏsᴇ', callback_data='close_data')
+            InlineKeyboardButton('💻 Visit Link', url='http://examplelink.com')
+        ], [
+            InlineKeyboardButton('Back to Home', callback_data='start'),
+            InlineKeyboardButton('🔒 Close', callback_data='close_data')
         ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text="🔗 Here's your link to visit:",
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+    
+    # Handle /base_site command
+    elif query.data == "base_site":
+        # Your base_site logic here
+        buttons = [[
+            InlineKeyboardButton('Back to Home', callback_data='start'),
+            InlineKeyboardButton('🔒 Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text="🔧 Configure your base site here.",
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+
+    # Handle /api command
+    elif query.data == "api":
+        buttons = [[
+            InlineKeyboardButton('Back to Home', callback_data='start'),
+            InlineKeyboardButton('🔒 Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text="🛠 Configure your API settings.",
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+
+    # Handle /deletecloned command
+    elif query.data == "deletecloned":
+        buttons = [[
+            InlineKeyboardButton('Back to Home', callback_data='start'),
+            InlineKeyboardButton('🔒 Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text="⚠️ Delete your cloned bot settings.",
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+
+    # Handle /broadcast command
+    elif query.data == "broadcast":
+        buttons = [[
+            InlineKeyboardButton('Back to Home', callback_data='start'),
+            InlineKeyboardButton('🔒 Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text="📣 Broadcast your message to all users.",
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+
+    # Other callback queries
+    elif query.data == "help":
+        # Show help message
+        buttons = [[
+            InlineKeyboardButton('Home', callback_data='start'),
+            InlineKeyboardButton('Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
         await client.edit_message_media(
             query.message.chat.id, 
             query.message.id, 
             InputMediaPhoto(random.choice(PICS))
         )
-        reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.CHELP_TXT,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
-        )  
-
-    elif query.data == "about":
-        buttons = [[
-            InlineKeyboardButton('Hᴏᴍᴇ', callback_data='start'),
-            InlineKeyboardButton('🔒 Cʟᴏsᴇ', callback_data='close_data')
-        ]]
-        await client.edit_message_media(
-            query.message.chat.id, 
-            query.message.id, 
-            InputMediaPhoto(random.choice(PICS))
         )
-        me2 = (await client.get_me()).mention
-        id = client.me.id
-        owner = mongo_db.bots.find_one({'bot_id': id})
-        ownerid = int(owner['user_id'])
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.CABOUT_TXT.format(me2, ownerid),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )  
-
-# This file is part of < https://github.com/IamDvis/DV-FILESTORE > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-FILESTORE/blob/master/LICENSE >
